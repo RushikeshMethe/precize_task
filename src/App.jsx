077f402aad118ab64a3421b968f3ef038bf5c828
rushikesh-methe <metherushi1@gmail.com>
2023-10-06 09:29:07 +0530

    UI added

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,23 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='Operations'>
+      <h3> CRUD operations</h3>
+      <label>Name :</label> <input type='text'></input> <br></br>
+      <label>Address :</label> <input type='text'></input> <br></br>
+      <label>City :</label> <input type='text'></input> <br></br>
+      <label>Country :</label> <input type='text'></input> <br></br>
+      <label>Pincode :</label> <input type='number'></input> <br></br>
+      <label>SAT Score :</label> <input type='Number'></input> <br></br>
+    </div>
+    <div className='options'>
+      <button className='btn' >Insert Data</button>
+      <button className='btn' >View Data</button>
+      <button className='btn' >Get Rank</button>
+      <button className='btn' >Update Score</button>
+      <button className='btn' >Delete Record</button>
+    </div>
+    <div className='Result'></div>
     </>
   )
 }
